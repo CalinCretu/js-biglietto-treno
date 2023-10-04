204e4d02616e53e3bb620f123b8e8ff879d29fa1
CalinCretu <calincretu1412@gmail.com>
2023-10-04 15:40:31 +0200

    ADD - working_exercise

---
+++ README.md
@@ -0,0 +1,15 @@
+# Calcolo del prezzo del biglietto del treno
+
+- Richiedere informazioni al cliente:
+    - chiedere all'utente il numero di chilometri che vuole percorrere
+        - creare variabile con valore restituito da prompt
+    - chidere all'utente l'età del passseggero
+        - creare variabile con valore restituito da prompt
+- Creare le variabili utilizzate per calcolare il prezzo totale del viaggio:
+    - creare variabile con il prezzo base definito di 0.21 euro al km
+    - creare variabile con sconto di 20% applicato se il cliente ha meno di 18 anni
+    - creare variabile con sconto di 40% applicato se il cliente ha piu' di 65 anni
+- Calcolare il prezzo finale del biglietto a seconda dei prompt:
+    - creare condizione con cliente di eta' inferiore a 18 anni
+    - creare condizione con cliente di eta' superiore a 65 anni
+    - creare condizione con cliente di eta' compresa tra 18 e 65 anni
---
+++ js/app.js
@@ -0,0 +1,43 @@
+// Richiedere informazioni al cliente:
+// - chiedere all'utente il numero di chilometri che vuole percorrere
+//  - creare variabile con valore restituito da prompt
+let distance = prompt('How many KM are you willing to travel?');
+console.log(distance);
+
+// - chidere all'utente l'età del passseggero
+//  - creare variabile con valore restituito da prompt
+let age = prompt('How OLD is the passenger?');
+console.log(age);
+
+// Creare le variabili utilizzate per calcolare il prezzo totale del viaggio:
+//  - creare variabile con il prezzo base definito di 0.21 euro al km
+let basePrice = Math.floor(0.21 * distance);
+console.log(basePrice);
+
+// - creare variabile con sconto di 20% applicato se il cliente ha meno di 18 anni
+let discount20 = Math.floor(basePrice - basePrice * 0.2);
+console.log(discount20);
+
+// - creare variabile con sconto di 40% applicato se il cliente ha piu' di 65 anni
+let discount40 = Math.floor(basePrice - basePrice * 0.4);
+console.log(discount40);
+
+// - Calcolare il prezzo finale del biglietto a seconda dei prompt:
+//  - creare condizione con cliente di eta' inferiore a 18 anni
+if (age <= 18) {
+    const messageDomElement = document.getElementById('price');
+    messageDomElement.innerHTML = discount20;
+    console.log(discount20)
+}
+// creare condizione con cliente di eta' superiore a 65 anni
+else if (age >= 65) {
+    const messageDomElement = document.getElementById('price');
+    messageDomElement.innerHTML = discount40;
+    console.log(discount40)
+}
+// - creare condizione con cliente di eta' compresa tra 18 e 65 anni
+else {
+    const messageDomElement = document.getElementById('price');
+    messageDomElement.innerHTML = basePrice;
+    console.log(basePrice)
+}
